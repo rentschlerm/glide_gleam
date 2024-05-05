249a5cf262aa7bf7dee8dfb4d05dd19c2beeee9f
iverson sotto <bersotto18@gmail.com>
2024-05-05 20:28:27 +0800

    ss

--- carwash.sql
+++ carwash.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2024 at 12:55 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: May 05, 2024 at 02:25 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,11 @@ CREATE TABLE `accounts` (
 INSERT INTO `accounts` (`account_id`, `email`, `password`, `type`, `status`) VALUES
 (39, 'vehicleowner@gmail.com', '123', '2', 'Accept re'),
 (40, 'shopowner@gmail.com', '123', '1', 'Accept re'),
-(41, 'testshop@gmail.com', '123', '1', 'Accept re');
+(41, 'testshop@gmail.com', '123', '1', 'Accept re'),
+(42, '', '', '1', 'Accept re'),
+(43, '', '', '1', 'Accept re'),
+(44, 'bersotto18@gmail.com', '123', '1', 'Accept re'),
+(45, 'shopowner@gmail.com', '123', '1', 'Accept re');
 
 -- --------------------------------------------------------
 
@@ -60,25 +64,95 @@ CREATE TABLE `appointment` (
   `service_id` int(5) NOT NULL,
   `vehicle_type` varchar(10) NOT NULL,
   `vehicle_size` varchar(7) NOT NULL,
-  `license_plate` varchar(15) NOT NULL
+  `license_plate` varchar(15) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `comments` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`appointment_id`, `vehicle_owner_id`, `shop_info_id`, `queue_number`, `appointment_date`, `status`, `service_id`, `vehicle_type`, `vehicle_size`, `license_plate`) VALUES
-(17, 39, 5, 1, '2024-04-22 08:00:00', 'Completed', 17, '', '', ''),
-(18, 39, 5, 2, '2024-04-22 09:00:00', 'Not Completed', 20, '', '', ''),
-(19, 39, 5, 3, '2024-04-22 08:00:00', 'Cancelled', 17, '', '', ''),
-(20, 39, 5, 4, '2024-04-22 10:00:00', 'Cancelled', 17, '', '', ''),
-(21, 39, 5, 5, '2024-04-22 11:00:00', 'Cancelled', 17, '', '', ''),
-(22, 39, 5, 6, '2024-04-23 12:00:00', 'Completed', 25, '', '', ''),
-(23, 39, 5, 7, '2024-04-23 12:00:00', 'Completed', 33, '', '', ''),
-(24, 39, 5, 8, '2024-04-23 08:00:00', 'Completed', 17, '', '', ''),
-(25, 39, 5, 9, '2024-04-23 11:00:00', 'Completed', 17, '', '', ''),
-(26, 39, 5, 10, '2024-04-23 15:00:00', 'Not Completed', 36, '', '', ''),
-(27, 39, 5, 11, '2024-04-23 18:00:00', 'Not Completed', 20, '', '', '');
+INSERT INTO `appointment` (`appointment_id`, `vehicle_owner_id`, `shop_info_id`, `queue_number`, `appointment_date`, `status`, `service_id`, `vehicle_type`, `vehicle_size`, `license_plate`, `rating`, `comments`) VALUES
+(17, 39, 5, 1, '2024-04-22 08:00:00', 'Completed', 17, '', '', '', 1, 'sdsds'),
+(18, 39, 5, 2, '2024-04-22 09:00:00', 'Cancelled', 20, '', '', '', NULL, NULL),
+(19, 39, 5, 3, '2024-04-22 08:00:00', 'Cancelled', 17, '', '', '', NULL, NULL),
+(20, 39, 5, 4, '2024-04-22 10:00:00', 'Cancelled', 17, '', '', '', NULL, NULL),
+(21, 39, 5, 5, '2024-04-22 11:00:00', 'Cancelled', 17, '', '', '', NULL, NULL),
+(22, 39, 5, 6, '2024-04-23 12:00:00', 'Completed', 25, '', '', '', NULL, NULL),
+(23, 39, 5, 7, '2024-04-23 12:00:00', 'Completed', 33, '', '', '', NULL, NULL),
+(24, 39, 5, 8, '2024-04-23 08:00:00', 'Completed', 17, '', '', '', NULL, NULL),
+(25, 39, 5, 9, '2024-04-23 11:00:00', 'Completed', 17, '', '', '', NULL, NULL),
+(26, 39, 5, 10, '2024-04-23 15:00:00', 'Cancelled', 36, '', '', '', NULL, NULL),
+(27, 39, 5, 11, '2024-04-23 18:00:00', 'Cancelled', 20, '', '', '', NULL, NULL),
+(28, 39, 6, 1, '2024-04-27 12:00:00', 'Completed', 36, '', '', '', 1, 'sdsds'),
+(29, 39, 5, 12, '2024-05-01 17:00:00', 'Cancelled', 25, '', '', '', NULL, NULL),
+(30, 39, 6, 2, '2024-04-30 13:00:00', 'Completed', 23, '', '', '', NULL, NULL),
+(31, 41, 6, 3, '2024-04-30 13:00:00', 'Not Completed', 23, '', '', '', NULL, NULL),
+(32, 39, 6, 4, '2024-05-10 10:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(33, 39, 6, 5, '2024-04-30 11:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(37, 39, 6, 6, '2024-04-30 10:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(38, 39, 6, 7, '2024-04-29 10:00:00', 'Completed', 23, '', '', '', NULL, NULL),
+(39, 39, 6, 8, '2024-04-30 14:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(40, 39, 6, 9, '2024-04-29 08:00:00', 'Completed', 40, '', '', '', NULL, NULL),
+(41, 39, 6, 10, '2024-04-30 09:00:00', 'Completed', 23, '', '', '', NULL, NULL),
+(42, 39, 6, 11, '2024-04-29 14:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(43, 39, 6, 12, '2024-04-30 16:00:00', 'Completed', 23, '', '', '', NULL, NULL),
+(44, 39, 6, 13, '2024-04-29 15:00:00', 'Completed', 31, '', '', '', NULL, NULL),
+(45, 39, 6, 14, '2024-04-30 08:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(46, 39, 6, 15, '2024-04-30 09:00:00', 'Cancelled', 31, '', '', '', NULL, NULL),
+(47, 39, 6, 16, '2024-04-30 10:00:00', 'Completed', 23, '', '', '', NULL, NULL),
+(48, 39, 6, 17, '2024-04-30 11:00:00', 'Completed', 15, '', '', '', NULL, NULL),
+(49, 39, 6, 18, '2024-04-30 18:00:00', 'Cancelled', 15, '', '', '', NULL, NULL),
+(50, 39, 6, 19, '2024-05-22 10:00:00', 'Cancelled', 15, '', '', '', NULL, NULL),
+(51, 39, 6, 20, '2024-05-22 09:00:00', 'Cancelled', 31, '', '', '', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_history`
+--
+
+CREATE TABLE `appointment_history` (
+  `appointment_id` int(11) NOT NULL,
+  `customer_name` varchar(255) DEFAULT NULL,
+  `shop_name` varchar(255) DEFAULT NULL,
+  `queue_number` int(11) DEFAULT NULL,
+  `appointment_date` datetime DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment_history`
+--
+
+INSERT INTO `appointment_history` (`appointment_id`, `customer_name`, `shop_name`, `queue_number`, `appointment_date`, `status`) VALUES
+(50, NULL, NULL, 19, '2024-05-22 10:00:00', 'Cancelled'),
+(51, NULL, NULL, 20, '2024-05-22 09:00:00', 'Cancelled');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `feedback_id` int(11) NOT NULL,
+  `shop_info_id` int(11) NOT NULL,
+  `feedback` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `rating_id` int(11) NOT NULL,
+  `shop_info_id` int(11) NOT NULL,
+  `rating` decimal(2,1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -191,7 +265,11 @@ CREATE TABLE `shop_owners` (
 
 INSERT INTO `shop_owners` (`shop_owner_id`, `first_name`, `last_name`, `phone`) VALUES
 (40, 'Rentschler ', 'Capacite', 2147483647),
-(41, 'test', 'shop', 123456789);
+(41, 'test', 'shop', 123456789),
+(42, '', '', 0),
+(43, '', '', 0),
+(44, 'Iverson', 'Sotto', 2147483647),
+(45, 'Iverson', 'Sotto', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -239,6 +317,26 @@ ALTER TABLE `appointment`
   ADD KEY `shop_info_id` (`shop_info_id`);
 
 --
+-- Indexes for table `appointment_history`
+--
+ALTER TABLE `appointment_history`
+  ADD PRIMARY KEY (`appointment_id`);
+
+--
+-- Indexes for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD KEY `shop_info_id` (`shop_info_id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`rating_id`),
+  ADD KEY `shop_info_id` (`shop_info_id`);
+
+--
 -- Indexes for table `services`
 --
 ALTER TABLE `services`
@@ -272,13 +370,31 @@ ALTER TABLE `vehicle_owners`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `account_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `account_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `appointment_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `appointment_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT for table `appointment_history`
+--
+ALTER TABLE `appointment_history`
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `services`
@@ -290,13 +406,13 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT for table `shop_info`
 --
 ALTER TABLE `shop_info`
-  MODIFY `shop_info_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `shop_info_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `shop_owners`
 --
 ALTER TABLE `shop_owners`
-  MODIFY `shop_owner_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `shop_owner_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `vehicle_owners`
@@ -313,6 +429,18 @@ ALTER TABLE `vehicle_owners`
 --
 ALTER TABLE `appointment`
   ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`shop_info_id`) REFERENCES `shop_info` (`shop_info_id`);
+
+--
+-- Constraints for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD CONSTRAINT `feedbacks_ibfk_1` FOREIGN KEY (`shop_info_id`) REFERENCES `shop_info` (`shop_info_id`);
+
+--
+-- Constraints for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`shop_info_id`) REFERENCES `shop_info` (`shop_info_id`);
 
 --
 -- Constraints for table `shop_info`
